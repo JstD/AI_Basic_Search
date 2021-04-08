878b64c74cce348c62c1e10b151f98f517a3fd17
jstD <dung.truong2000@hcmut.edu.vn>
2021-04-08 22:51:57 +0700

    update

--- FindPath/path_finding_astar.py
+++ FindPath/path_finding_astar.py
@@ -1,255 +1,142 @@
-import pygame
-import math
-from queue import PriorityQueue
-
-# display
-WIDTH = 800
-WIN = pygame.display.set_mode((WIDTH, WIDTH))
-pygame.display.set_caption("A* Path Finding Algorithm")
-
-# colors
-RED = (255,0,0)
-GREEN = (0,255,0)
-BLUE = (0,0,255)
-YELLOW = (255,255,0)
-WHITE = (255,255,255)
-BLACK = (0,0,0)
-PURPLE = (128,0,128)
-ORANGE = (255,165,0)
-GREY = (128,128,128)
-TURQUOISE = (64,224,208)
-
-
-class Spot:
-	def __init__(self, row, col, width, total_rows):
-		self.row = row
-		self.col = col
-		self.x = row * width
-		self.y = col * width
-		self.color = WHITE
-		self.neighbors = []
-		self.width = width
-		self.total_rows = total_rows
-
-	def get_pos(self):
-		return self.row, self.col
-
-	def is_closed(self):
-		return self.color == RED
-
-	def is_open(self):
-		return self.color == GREEN
-
-	def is_barrier(self):
-		return self.color == BLACK
-
-	def is_start(self):
-		return self.color == ORANGE
-
-	def is_end(self):
-		return self.color == TURQUOISE
-
-	def reset(self):
-		return self.color == WHITE
-
-	def  make_start(self):
-		self.color = ORANGE
-
-	def make_closed(self):
-		self.color = RED
-
-	def make_open(self):
-		self.color = GREEN
-
-	def make_barrier(self):
-		self.color = BLACK
-
-	def make_end(self):
-		self.color = TURQUOISE
-
-	def make_path(self):
-		self.color = PURPLE
-
-	def draw(self, win):
-		pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.width))
-
-	def update_neighbors(self, grid):
-		self.neighbors = []
-		if self.row < self.total_rows - 1 and not grid[self.row + 1][self.col].is_barrier(): # Down
-			self.neighbors.append(grid[self.row + 1][self.col])
-
-		if self.row > 0 and not grid[self.row - 1][self.col].is_barrier(): # Up
-			self.neighbors.append(grid[self.row - 1][self.col])
-
-		if self.col < self.total_rows - 1 and not grid[self.row][self.col + 1].is_barrier(): # Left
-			self.neighbors.append(grid[self.row][self.col + 1])
-
-		if self.row > 0 and not grid[self.row][self.col - 1].is_barrier(): # Right
-			self.neighbors.append(grid[self.row][self.col - 1])
-
-	def __lt__(self, other):
-		return False
-
-
-def h(p1, p2): # heuristic function
-	x1, y1 = p1
-	x2, y2 = p2
-	return abs(x1 - x2) + abs(y1 - y2)
-
-
-def reconstruct_path(came_from, current, draw):
-	while current in came_from:
-		current = came_from[current]
-		current.make_path()
-		draw()
-
-
-def algorithm(draw, grid, start, end):
-	count = 0
-	open_set = PriorityQueue() # We cant check whats in the queue even though we can insert and pop spots from the queue
-	open_set.put((0, count, start))
-	came_from = {}
-	g_score = {spot: float("inf") for row in grid for spot in row} # assigning the g_score to infinite for every spot in the grid
-	g_score[start] = 0
-	f_score = {spot: float("inf") for row in grid for spot in row} # assigning the f_score to infinite for every spot in the grid
-	f_score[start] = h(start.get_pos(), end.get_pos())
-
-	open_set_hash = {start} # We can check whats in the queue. So thats why we created this dict
-
-	while not open_set.empty():
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				pygame.quit()
-
-		current = open_set.get()[2]
-		open_set_hash.remove(current)
-
-		if current == end:
-			reconstruct_path(came_from, end, draw)
-			end.make_end()
-			return True
-
-		for neighbor in current.neighbors:
-			temp_g_score = g_score[current] + 1
-
-			if temp_g_score < g_score[neighbor]:
-				came_from[neighbor] = current
-				g_score[neighbor] = temp_g_score
-				f_score[neighbor] = temp_g_score + h(neighbor.get_pos(), end.get_pos())
-				
-				if neighbor not in open_set_hash:
-					count += 1
-					open_set.put((f_score[neighbor], count, neighbor))
-					open_set_hash.add(neighbor)
-					neighbor.make_open()
-
-		draw()
-		
-		if current != start:
-			current.make_closed()	
-
-	return False				
-
-
-def make_grid(rows, width):
-	grid = []
-	gap = width // rows # gap stands for the rectangle's width
-
-	for i in range(rows):
-		grid.append([])
-		for j in range(rows):
-			spot = Spot(i, j, gap, rows)
-			grid[i].append(spot)
-
-	return grid
-
-
-def draw_grid(win, rows, width):
-	gap = width // rows
-
-	for i in range(rows):
-		pygame.draw.line(win, GREY, (0, i * gap), (width, i * gap))
-		for j in range(rows):
-			pygame.draw.line(win, GREY, (j * gap, 0), (j * gap, width))
-
-
-def draw(win, grid, rows, width):
-	win.fill(WHITE)
-
-	for row in grid:
-		for spot in row:
-			spot.draw(win)
-
-	draw_grid(win, rows, width)
-	pygame.display.update()
-
-
-def get_clicked_pos(pos, rows, width):
-	gap = width // rows
-	y, x = pos
-
-	row = y//gap
-	col = x//gap
-
-	return row, col
-
-
-def main(win, width):
-	ROWS = 50 # how many cubes you want in your grid
-	grid = make_grid(ROWS, width)
-
-	start = None
-	end = None
-
-	run = True
-
-	while run:
-		draw(win, grid, ROWS, width)
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				run = False
-
-			if pygame.mouse.get_pressed()[0]: # Left mouse button
-				pos = pygame.mouse.get_pos()
-				row, col = get_clicked_pos(pos, ROWS, width)
-				spot = grid[row][col]
-				
-				if not start and spot != end:
-					start = spot
-					start.make_start()
-
-				elif not end and spot != start:
-					end = spot
-					end.make_end()
-
-				elif spot != end and spot != start:
-					spot.make_barrier()
-
-			elif pygame.mouse.get_pressed()[2]: # Right mouse button				
-				pos = pygame.mouse.get_pos()
-				row, col = get_clicked_pos(pos, ROWS, width)
-				spot = grid[row][col]
-				spot.reset()
-				if spot == start:
-					start = None
-				elif spot == end:
-					end = None
-
-			if event.type == pygame.KEYDOWN:
-				if event.key == pygame.K_SPACE and start and end:
-					for row in grid:
-						for spot in row:
-							spot.update_neighbors(grid)
-
-					algorithm(lambda: draw(win, grid, ROWS, width), grid, start, end)
-
-				if event.key == pygame.K_c:
-					start = None
-					end = None
-					grid = make_grid(ROWS, width)
-
-
-	pygame.quit()
-
-
-main(WIN, WIDTH)
+import numpy as np
+from heapq import heappush, heappop
+import matplotlib
+matplotlib.use('TKAgg')
+from animation import draw
+import argparse
+
+
+class Node():
+    """
+    cost_from_start - the cost of reaching this node from the starting node
+    state - the state (row,col)
+    parent - the parent node of this node, default as None
+    """
+    def __init__(self, state, cost_from_start, parent = None):
+        self.state = state
+        self.parent = parent
+        self.cost_from_start = cost_from_start
+
+
+class Maze():
+    
+    def __init__(self, map, start_state, goal_state, map_index):
+        self.start_state = start_state
+        self.goal_state = goal_state
+        self.map = map
+        self.visited = [] # state
+        self.m, self.n = map.shape 
+        self.map_index = map_index
+
+
+    def draw(self, node):
+        path=[]
+        while node.parent:
+            path.append(node.state)
+            node = node.parent
+        path.append(self.start_state)
+    
+        draw(self.map, path[::-1], self.map_index)
+
+
+    def goal_test(self, current_state):        
+        if np.array_equal(current_state, self.goal_state) is True:
+            return True
+        else:
+            return False
+
+
+    def get_cost(self, current_state, next_state):        
+        return 1
+        
+
+
+    def get_successors(self, state):
+        successors = []
+        directions = [(0, -1), (0, 1), (-1, 0), (1, 0)]
+        newState = state
+        for position in directions:
+            row, column = newState
+            nodePosition = (row + position[0], column + position[1])
+            
+            if int(nodePosition[0]) > (len(self.map)-1) or nodePosition[0] < 0 or nodePosition[1] > len(self.map[len(self.map)-1]) or nodePosition[1] <0:
+                continue
+
+            if self.map[nodePosition[0]][nodePosition[1]] == 0:
+                continue
+
+            self.map[nodePosition] = 0.5
+            successors.append(nodePosition)
+        
+        return successors
+
+    # heuristics function
+    def heuristics(self, state):
+        hCost = ((state[0]- self.goal_state[0])**2)+((state[1]- self.goal_state[1])**2)
+        return hCost
+
+
+    # priority of node 
+    def priority(self, node):
+        priority = node.cost_from_start + self.heuristics(node.state)
+        return priority
+        
+    # solve it
+    def solve(self):
+        state = self.start_state
+        currentNode = Node(state,0, None)
+        count = 1
+        self.visited.append(state)
+        
+        openList = []
+        heappush(openList, (self.priority(currentNode), count, currentNode))
+
+        while openList:
+            currentNode = heappop(openList)[2]
+
+            if self.goal_test(currentNode.state) is True:
+                self.draw(currentNode)
+
+            successors = self.get_successors(currentNode.state)
+
+            for nextState in successors:
+                isVisited = False
+
+                for element in self.visited:
+                    if np.array_equal(nextState, element):
+                        isVisited = True
+                
+                if isVisited is False:
+                    gCost = currentNode.cost_from_start + self.get_cost(currentNode.state, nextState)                    
+                    nextNode = Node(nextState, gCost, currentNode)
+
+                    if self.goal_test(nextState) is True:
+                        self.draw(nextNode)
+                        return 
+
+                    if nextNode not in openList:
+                        heappush(openList, (self.priority(nextNode), count, nextNode))
+                        count += 1
+                    elif currentNode.cost_from_start >= nextNode.cost_from_start:
+                        continue
+
+                    nextNode.cost_from_start =gCost
+                
+                    self.visited.append(currentNode.state)
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description='maze')
+    parser.add_argument('-index', dest='index', required = True, type = int)
+    index = parser.parse_args().index
+
+    # Example:
+    # Run this in the terminal solving map 1
+    #     python maze_astar.py -index 1
+    
+    data = np.load('map_'+str(index)+'.npz')
+    
+    map, start_state, goal_state = data['map'], tuple(data['start']), tuple(data['goal'])
+
+    game = Maze(map, start_state, goal_state, index)
+    game.solve()
