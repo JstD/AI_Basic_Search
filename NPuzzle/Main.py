4479674e7a2ff31f45cfbca9ef80dbcafaa6d01f
manhnq0508 <81633441+manhnq0508@users.noreply.github.com>
2021-04-09 23:06:41 +0700

    Add files via upload

--- NPuzzle/Main.py
+++ NPuzzle/Main.py
@@ -1,5 +1,5 @@
 from Search_Algorithms import DFS #BFS, DFS, Greedy, AStar_search
-from Search_Algorithms import Greedy
+
 #initial state
 n = int(input("Enter n\n"))
 print("Enter your" ,n,"*",n, "puzzle")
@@ -33,34 +33,15 @@ def solvable(puzzle): #check if initial state puzzle is solvable: number of inve
 from time import time
 
 if solvable(root):
-#    print("Solvable, please wait. \n")
-#    time1 = time()
-#    BFS_solution = BFS(root, n)
-#    BFS_time = time() - time1
-#    print('BFS Solution is ', BFS_solution[0])
-#    print('Number of explored nodes is ', BFS_solution[1])    
-#    print('BFS Time:', BFS_time , "\n")
 
-    # time2 = time()
-    # DFS_solution = DFS(root, n)
-    # DFS_time = time() - time2
-    # print('DFS Solution is ', DFS_solution[0])
-    # print('Number of explored nodes is ', DFS_solution[1])
-    # print('DFS Time:', DFS_time, "\n")  
+    time2 = time()
+    DFS_solution = DFS(root, n)
+    DFS_time = time() - time2
+    print('DFS Solution is ', DFS_solution[0])
+    print('Number of explored nodes is ', DFS_solution[1])
+    print('DFS Time:', DFS_time, "\n")  
 
-    time3 = time()
-    Greedy_solution = Greedy(root, n)
-    Greedy_time = time() - time3
-    print('Greedy Solution is ', Greedy_solution[0])
-    print('Number of explored nodes is ', Greedy_solution[1])   
-    print('Greedy Time:', Greedy_time , "\n")
-    
-    # time4 = time()
-    # AStar_solution = AStar_search(root, n)
-    # AStar_time = time() - time4
-    # print('A* Solution is ', AStar_solution[0])
-    # print('Number of explored nodes is ', AStar_solution[1])   
-    # print('A* Time:', AStar_time)
+
 
     
 else:
--- NPuzzle/Search_Algorithms.py
+++ NPuzzle/Search_Algorithms.py
@@ -5,33 +5,9 @@ from queue import LifoQueue
 
 
 #Depth-first Search with limited depth
-def DFS(given_state , n): 
-    root = State(given_state, None, None, 0, 0)
-    if root.test():
-        return root.solution()
-    frontier = LifoQueue()
-    frontier.put(root)
-    explored = []
-    
-    while not(frontier.empty()):
-        current_node = frontier.get()
-        max_depth = current_node.depth #current depth
-        explored.append(current_node.state)
-        
-        if max_depth == 70:
-            continue #go to the next branch
 
-        children = current_node.expand(n)
-        for child in children:
-            if child.state not in explored:
-                if child.test():
-                    return child.solution(), len(explored)
-                frontier.put(child)
-    return (("Couldn't find solution in the limited depth."), len(explored))
-        
-    
  
-def Greedy(given_state , n):
+def DFS(given_state , n):
     frontier = PriorityQueue()
     explored = [] 
     counter = 0
